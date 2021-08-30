b378bb40eef97d1139e8b0227267c29bbe12f617
Max <maxnvdm@gmail.com>
2021-08-30 04:11:31 -0700

    adding frequency and amplitude measurements

--- oscMeasureNoise.py
+++ oscMeasureNoise.py
@@ -25,20 +25,27 @@ osc.write('AFG:AMPLitude ' + str(amplitude))
 def record_duration(duration, arr):
     for x in range(duration):
         osc.write('DVM:SOUrce CH1')
-        time.sleep(1)
+        time.sleep(1.5)
         timestamp1 = time.time() + 2082844800
         ch1 = float(osc.query('DVM:MEASU:VAL?'))
         row1 = np.array([round(timestamp1, 2), 'CH1', ch1])
 
         # time.sleep(0.3)
         osc.write('DVM:SOUrce CH2')
-        time.sleep(1)
+        time.sleep(1.5)
         timestamp2 = time.time() + 2082844800
         ch2 = float(osc.query('DVM:MEASU:VAL?'))
         row2 = np.array([round(timestamp2, 2), 'CH2', round(ch2, 2)])
 
+        freq_read = float(osc.query('AFG:FREQuency?'))
+        time.sleep(1.5)
+        amp_read = float(osc.query('AFG:AMPLitude?'))
+        timestamp3 = time.time() + 2082844800
+        row3 = np.array([round(timestamp3, 2), 'Frequency', freq_read, 'Amplitude', amp_read])
+
         arr = np.append(arr, [row1], axis=0)
         arr = np.append(arr, [row2], axis=0)
+        arr = np.append(arr, [row3], axis=0)
         print(arr)
         # time.sleep(0.2)
     return arr
@@ -59,9 +66,16 @@ def loop_infinite(arr):
             timestamp2 = time.time() + 2082844800
             ch2 = float(osc.query('DVM:MEASU:VAL?'))
             row2 = np.array([round(timestamp2, 2), 'CH2', ch2])
+            
+            freq_read = float(osc.query('AFG:FREQuency?'))
+            time.sleep(1.5)
+            amp_read = float(osc.query('AFG:AMPLitude?'))
+            timestamp3 = time.time() + 2082844800
+            row3 = np.array([round(timestamp3, 2), 'Frequency', freq_read, 'Amplitude', amp_read])
 
             arr = np.append(arr, [row1], axis=0)
             arr = np.append(arr, [row2], axis=0)
+            arr = np.append(arr, [row3], axis=0)
             print(arr)
     except KeyboardInterrupt:
         return arr
