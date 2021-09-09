d0c5ed4edaffd4ac7bd9c75dc5d353ffbbac9ec5
Merwe, Max Nikoi van der <max.van.der.merwe@desy.de>
2021-09-09 15:14:49 +0200

    frequency sweep tests updated

--- oscFrequencyTest.py
+++ oscFrequencyTest.py
@@ -4,83 +4,113 @@ import time
 import numpy as np
 import safetyChecks
 
+# Setup pyvisa comms with oscilloscope
 rm = pyvisa.ResourceManager()
 osc = rm.open_resource('TCPIP0::137.158.93.119::inst0::INSTR')
 
-testStartTime = time.strftime("%H:%M:%S", time.localtime())
-data = np.array([('Timestamp', 'Offset', 'Ch1 reading', 'Ch2 reading', 'AFG frequency', 'AFG amplitude')])
+# Create list of frequency ranges to sweep through
+low_frequencies = list(range(0,1000,10))
+low_frequencies[0] = 1
+mid_frequencies = list(range(1000, 10000, 100))
+high_frequencies = list(range(10000, 100000, 1000))
+all_frequencies = low_frequencies + mid_frequencies + high_frequencies
 
-waves_1 = {"SINE": {"offset": 1.2, "amplitude": 0.05,
-                    "low_freq_start": 1, "low_freq_end": 1000, "low_freq_step": 10,
-                    "mid_freq_start": 1000, "mid_freq_end": 10000, "mid_freq_step": 100,
-                    "high_freq_start": 10000, "high_freq_end": 300000, "high_freq_step": 1000},
-           "RAMP": {"offset": 1.17, "amplitude": 0.1},
-           "SINC": {"offset": 1.1, "amplitude": 0.1},
-           "SQUARE": {"offset": 1.17, "amplitude": 0.1}}
-waves_2 = {"SINE": {"offset": 2.2, "amplitude": 0.05,
-                    "low_freq_start": 1, "low_freq_end": 1000, "low_freq_step": 10},
-           "RAMP": {"offset": 2.2, "amplitude": 0.05},
-           "SINC": {"offset": 2.2, "amplitude": 0.05},
-           "SQUARE": {"offset": 2.2, "amplitude": 0.05}}
+# Create dictionaries for waveforms and their config
+waves_1 = {"SINE": {"offset": 1.212, "amplitude": 0.15},
+           "RAMP": {"offset": 1.212, "amplitude": 0.15},
+           "SINC": {"offset": 1.175, "amplitude": 0.14},
+           "SQUARE": {"offset": 1.212, "amplitude": 0.15}}
+waves_2 = {"SINE": {"offset": 2.345, "amplitude": 0.45},
+           "RAMP": {"offset": 2.345, "amplitude": 0.5},
+           "SINC": {"offset": 2.15, "amplitude": 0.45},
+           "SQUARE": {"offset": 2.322, "amplitude": 0.45}}
 
-waves_line = waves_2
+# Select 1.2 or 2.5 waveforms
+line = 2
+if line == 2:
+    waves_line = waves_2
+elif line == 1:
+    waves_line = waves_1
 
-waveforms = list(waves_line.keys())
-waveform = waveforms[0]
-freq_start = waves_line[waveform]["low_freq_start"]
-freq_end = waves_line[waveform]["low_freq_end"]
-freq_step = waves_line[waveform]["low_freq_step"]
-offset = waves_line[waveform]["offset"]
-amplitude = waves_line[waveform]["amplitude"]
-
+# Initial waveform settings
 if waves_line == waves_1:
-    safetyChecks.amplitude_check1_2(amplitude)
-    safetyChecks.offset_check1_2(offset)
+    offset = safetyChecks.safe_values1_2["offset"]
+    amplitude = safetyChecks.safe_values1_2["amplitude"]
+    amplitude = safetyChecks.amplitude_check1_2(amplitude)
+    offset = safetyChecks.offset_check1_2(offset)
 else:
-    safetyChecks.amplitude_check2_5(amplitude)
-    safetyChecks.offset_check2_5(offset)
+    offset = safetyChecks.safe_values2_5["offset"]
+    amplitude = safetyChecks.safe_values2_5["amplitude"]
+    amplitude = safetyChecks.amplitude_check2_5(amplitude)
+    offset = safetyChecks.offset_check2_5(offset)
 
-osc.write('AFG:FUNCtion ' + str(waveform))
-osc.write('AFG:FREQuency ' + str(freq_start))
+osc.write('AFG:FUNCtion DC')
+osc.write('AFG:FREQuency 1')
 osc.write('AFG:OFFSet ' + str(offset))
 osc.write('AFG:AMPLitude ' + str(amplitude))
 osc.write('AFG:OUTPut:STATE ON')
 
 testStartTime = time.strftime("%H:%M:%S", time.localtime())
-data = np.array([('Timestamp', 'Offset', 'Ch1 reading', 'Ch2 reading', 'AFG frequency', 'AFG amplitude')])
+data = np.array([('Timestamp start', 'Timestamp end' 'Offset', 'Ch1 reading', 'Ch2 reading', 'AFG frequency',
+                  'AFG amplitude', 'maximum1', 'maximum2', 'minimum1', 'minimum2', 'mean1', 'mean2', 'pkpk1', 'pkpk2')])
 
 
-def sweep_frequency(freq_start, freq_end, freq_step, arr):
+def sweep_frequency(waves_line, frequencies, arr, line):
     print("Starting frequency sweep test")
-    frequencies = [f for f in np.arange(freq_start, freq_end, freq_step)]
-    for freq in frequencies:
-        osc.write('DVM:SOUrce CH1')
-        time.sleep(1.25)
-        timestamp1 = time.time() + 2082844800
-        ch1 = float(osc.query('DVM:MEASU:VAL?'))
+    for wave in list(waves_line.keys()):
+        input("About to move onto next waveform, press any key to continue")
+        osc.write('AFG:FUNCtion ' + str(wave))
 
-        if ch1 >= 1.29:
-            osc.write('AFG:OFFSet ' + str(1))
-            print("Voltage reached 1.29, test reset to safe levels")
-            return arr
+        if line == 1:
+            wave_offset = safetyChecks.offset_check1_2(waves_line[wave]["offset"])
+            wave_amplitude = safetyChecks.amplitude_check1_2(waves_line[wave]["offset"])
+        elif line == 2:
+            wave_offset = safetyChecks.offset_check2_5(waves_line[wave]["offset"])
+            wave_amplitude = safetyChecks.amplitude_check2_5(waves_line[wave]["offset"])
 
-        osc.write('DVM:SOUrce CH2')
-        time.sleep(1.25)
-        ch2 = float(osc.query('DVM:MEASU:VAL?'))
+        osc.write('AFG:OFFSet ' + str(wave_offset))
+        osc.write('AFG:AMPLitude ' + str(wave_amplitude))
 
-        if ch2 >= 2.71:
-            osc.write('AFG:OFFSet ' + str(2.1))
-            print("Voltage reached 2.71, test reset to safe levels")
-            return arr
+        for freq in frequencies:
+            osc.write('AFG:FREQuency ' + str(freq))
+            timestamp1 = time.time() + 2082844800
+            time.sleep(3)
+            osc.write('DVM:SOUrce CH1')
+            time.sleep(1.25)
+            ch1 = float(osc.query('DVM:MEASU:VAL?'))
 
-        freq_read = float(osc.query('AFG:FREQuency?'))
-        amp_read = float(osc.query('AFG:AMPLitude?'))
+            if line == 1:
+                if ch1 >= 1.29:
+                    osc.write('AFG:OFFSet ' + str(1.17))
+                    print("Voltage reached 1.29, test reset to safe levels")
+                    return arr
 
-        row = np.array([round(timestamp1, 2), offset, ch1, ch2, freq_read, amp_read])
-        arr = np.append(arr, [row], axis=0)
-        print(arr)
+            osc.write('DVM:SOUrce CH2')
+            time.sleep(1.25)
+            ch2 = float(osc.query('DVM:MEASU:VAL?'))
 
-        osc.write('AFG:FREQuency ' + str(freq))
+            if line == 2:
+                if ch2 >= 2.71:
+                    osc.write('AFG:OFFSet ' + str(2.15))
+                    print("Voltage reached 2.71, test reset to safe levels")
+                    return arr
+
+            freq_read = float(osc.query('AFG:FREQuency?'))
+            amp_read = float(osc.query('AFG:AMPLitude?'))
+            measurements = safetyChecks.measurements(osc)
+            timestamp2 = time.time() + 2082844800
+            row = np.array([round(timestamp1, 2), round(timestamp2, 2), wave_offset, ch1, ch2, freq_read, amp_read] + measurements)
+            arr = np.append(arr, [row], axis=0)
+            print(arr)
+        # Writing safe values before changing waveforms
+        if line == 1:
+            wave_offset = safetyChecks.safe_values1_2["offset"]
+            wave_amplitude = safetyChecks.safe_values1_2["amplitude"]
+        elif line == 2:
+            wave_offset = safetyChecks.safe_values2_5["offset"]
+            wave_amplitude = safetyChecks.safe_values2_5["amplitude"]
+        osc.write('AFG:OFFSet ' + str(wave_offset))
+        osc.write('AFG:AMPLitude ' + str(wave_amplitude))
     return arr
 
 
@@ -138,8 +168,7 @@ def test_settings(waveforms, arr):
     return arr
 
 
-config_settings = test_settings(waveforms, data)
-# dataRecord = sweep_frequency(freq_start, freq_end, freq_step, data)
-# pd.DataFrame(dataRecord).to_csv('oscData\\oscilloscope' + str(offset) + str(waveform) + 'start' + str(freq_start) + 'end'
-#                                 + str(freq_end) + 'time' + str(testStartTime).replace(':', '-') + '.csv')
-pd.DataFrame(config_settings).to_csv('oscData\\testSettings' + str(testStartTime).replace(':', '-') + '.csv')
+# config_settings = test_settings(waveforms, data)
+dataRecord = sweep_frequency(waves_line, data, line)
+pd.DataFrame(dataRecord).to_csv('oscData\\oscilloscopeFrequencySweep' + str(line) + str(testStartTime).replace(':', '-') + '.csv')
+# pd.DataFrame(config_settings).to_csv('oscData\\testSettings' + str(testStartTime).replace(':', '-') + '.csv')
